832c8b0f229a3ba1754ab2993a15fb7eb03eaeb3
lpmor22 <laisepaixao@live.com>
2019-03-24 20:33:19 -0300

    analise-primer v0.7 python

--- analise-primer.py
+++ analise-primer.py
@@ -1,29 +1,29 @@
 # coding=utf-8
 import os
+import sys
 
-enunciado = ('''-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+enunciado = ('''-------------------------------------------------------------------------------------------------------------------------------------------
 
-                                                                                             analise-primer v0.6                                                                                             
+                                                            analise-primer v0.7                                                            
 
--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------------------------------------------------------
 
-
-                                                               3'-CTTCCTACAAGGGAAGGCCAGGGAATTTTCTTCAGAGCAGACCAGAGCCAACAGCCCCACCAGAAGAGAGCTTCAGGTC-5'
-                                                               5'-GAAGGATGTTCCCTTCCGGTCCCTTAAAAGAAGTCTCGTCTGGTCTCGGTTGTCGGGGTGGTCTTCTCTCGAAGTCCAG-3'
-
-
-                                                               3'-CTTCCTACAAGGGAAGGCCAGGGAATTTTCTTCAGAGCAGACCAGAGCCAACAGCCCCACCAGAAGAGAGCTTCAGGTC-5'
-                                                                               |||||||||||||||||
-                                                                            5'-TTCCGGTCCCTTAAAAG-3' --> primer 1
-
-                                                                                                          primer 2 <-- 3'-CCACCAGAAGAGAGCTTC-5'
-                                                                                                                          ||||||||||||||||||
-                                                               5'-GAAGGATGTTCCCTTCCGGTCCCTTAAAAGAAGTCTCGTCTGGTCTCGGTTGTCGGGGTGGTCTTCTCTCGAAGTCCAG-3'
+                           3'-CTTCCTACAAGGGAAGGCCAGGGAATTTTCTTCAGAGCAGACCAGAGCCAACAGCCCCACCAGAAGAGAGCTTCAGGTC-5'                           
+                           5'-GAAGGATGTTCCCTTCCGGTCCCTTAAAAGAAGTCTCGTCTGGTCTCGGTTGTCGGGGTGGTCTTCTCTCGAAGTCCAG-3'
 
 
--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+                           3'-CTTCCTACAAGGGAAGGCCAGGGAATTTTCTTCAGAGCAGACCAGAGCCAACAGCCCCACCAGAAGAGAGCTTCAGGTC-5'
+                                           |||||||||||||||||
+                                        5'-TTCCGGTCCCTTAAAAG-3' --> primer 1
 
--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+                                                                      primer 2 <-- 3'-CCACCAGAAGAGAGCTTC-5'
+                                                                                      ||||||||||||||||||
+                           5'-GAAGGATGTTCCCTTCCGGTCCCTTAAAAGAAGTCTCGTCTGGTCTCGGTTGTCGGGGTGGTCTTCTCTCGAAGTCCAG-3'
+
+
+-------------------------------------------------------------------------------------------------------------------------------------------
+
+-------------------------------------------------------------------------------------------------------------------------------------------
 
 PCR E PRIMERS
 
@@ -34,7 +34,7 @@ PRIMERS são oligonucleotídeos sintetizados quimicamente com base na sequência
 O primer FORWARD (ou SENSO) é o primer que hibridiza com fita molde sentido 5'-3'. Ou seja, quando o primer é construído, o desenho do primer é criado em relação à fita molde.
 O primer REVERSE (ou ANTI-SENSO) é aquele primer que hibridiza com a fita molde sentido 3'-5'. Ou seja, quando é construído, o desenho do primer é em relação ao COMPLEMENTO-REVERSO da fita molde.
 
--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------------------------------------------------------
 
 REGRAS GERAIS
 
@@ -58,18 +58,18 @@ O desenvolvimento de PRIMERS requer que alguns critérios sejam respeitados:
 * A Ta é a temperatura em que os primers começam a hibridizar na fita molde. O valor da Ta é até 5°C abaixo da Tm.
 
 5- TERMINAÇÕES NÃO DEVEM SER COMPLEMENTARES (HAIRPIN).
-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.
+* Os primers não devem conter regiões de complementariedade com as suas terminações 5' e/ou 3'. Caso isto ocorra, o primer forma um grampo (HAIRPIN), impedindo a hibridização com a fita molde.
 
 6- PRIMERS NÃO DEVEM SER AUTO-COMPLEMENTARES (SELF DIMER).
-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.
+* Os primers não devem conter regiões de complementariedade dentro de sua sequência. Caso isto ocorra, o primer hibridiza com ele mesmo (SELF-DIMER - outra molécula, porém igual), impedindo a hibridização com a fita molde.
 
 7- PRIMERS NÃO DEVEM SER COMPLEMENTARES ENTRE SI (PRIMER DIMER).
-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.
+* Os primers não devem conter regiões de complementariedade com o seu par. Caso isto ocorra, o primer hibridiza com o par (PRIMER-DIMER), impedindo a hibridização com a fita molde.
 
 8- PRIMERS NÃO DEVEM TERMINAR COM MAIS DE TRÊS BASES REPETIDAS, ESPECIALMENTE EM CASO DE C OU G.
-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.
+* Regiões com repetições muito longas aumentam o potencial de hibridização inespecífica (por exemplo, regiões ricas em guanina).
 
--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------------------------------------------------------
 
 SITUAÇÃO PROBLEMA
 
@@ -118,7 +118,7 @@ ACTAAAGAATTACAAAAACAAATTACAAAAATTCAAAATTTTCGGGTTTATTACAGGGACAGCAGAAATCCACTTTGGAA
 TGGAAAGGTGAAGGGGCAGTAGTAATACAAGATAATAGTGACATAAAAGTAGTGCCAAGAAGAAAAGCAAAGATCATTAGGGATTATGGAAAACAGATG
 GCAGGTGATGATTGTGTGGCAAGTAGACAGGATGAGGATTAG
 
--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------''')
+-------------------------------------------------------------------------------------------------------------------------------------------''')
 
 # regiões referente ao enunciado
 lista_regiao1 = "TTTTTTAGGGAAGATCTGGCCTTCCTACAAGGGAAGGCCAGGGAATTTTCTTCAGAGCAGACCAGAGCCAACAGCCCCACCAGAAGAGAGCTTCAGGTCTGGGGTAGAGACAACAA"
@@ -130,7 +130,8 @@ lista_regiao2_complemento_reverso = "CTAATCCTCATCCTGTCTACTTGCCACACAATCATCACCTGCC
 
 print(enunciado)  # mostrar o enunciado com a situação problema e informações para a construção de primers
 
-while True:
+primer_tests = True
+while primer_tests:
     primer1 = input('''
 A partir destes conhecimentos, crie o desenho para o primer FORWARD.
 ATENÇÃO! Sempre escrever o primer no sentido 5'-3'.
@@ -143,14 +144,12 @@ Desenho do primer FORWARD: ''').upper()  # pedir o desenho do primer1
     composicao_g_primer1 = primer1.count("G")  # determinar a composição de guanina do primer1
     composicao_t_primer1 = primer1.count("T")  # determinar a composição de timina do primer1
     composicao_a_primer1 = primer1.count("A")  # determinar a composição de adenina do primer1
-    porcentagem_gc_primer1 = (
-                                         composicao_c_primer1 + composicao_g_primer1) / tamanho_primer1 * 100  # determinar a composição de G+C do primer1
-    temperatura_melting_primer1 = (4 * (composicao_c_primer1 + composicao_g_primer1)) + (2 * (
-                composicao_a_primer1 + composicao_t_primer1))  # determinar a temperatura de melting (Tm) do primer1
+    porcentagem_gc_primer1 = (composicao_c_primer1 + composicao_g_primer1) / tamanho_primer1 * 100  # determinar a composição de G+C do primer1
+    temperatura_melting_primer1 = (4 * (composicao_c_primer1 + composicao_g_primer1)) + (2 * (composicao_a_primer1 + composicao_t_primer1))  # determinar a temperatura de melting (Tm) do primer1
     temperatura_min_hibridizacao_primer1 = temperatura_melting_primer1 - 5  # determinar a temperatura de hibridização (Ta) do primer1
     if encontrar_primer1 >= 0:
         print('''
--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------------------------------------------------------
 
 O seu primer FORWARD:
 ''')
@@ -166,7 +165,7 @@ O seu primer FORWARD:
                         print('''
 O primer FORWARD está OK!
 
--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------''')
+-------------------------------------------------------------------------------------------------------------------------------------------''')
                         primer2 = input('''
 Agora crie o desenho para o primer REVERSE.
 ATENÇÃO! Sempre escrever o primer no sentido 5'-3'.
@@ -174,22 +173,19 @@ ATENÇÃO! Sempre escrever o primer no sentido 5'-3'.
 O site https://www.bioinformatics.org/sms/rev_comp.html pode ajudar para determinar o COMPLEMENTO-REVERSO da região de interesse.
 
 Desenho do primer REVERSE: ''').upper()  # pedir o desenho do primer2
-                        encontrar_primer2 = lista_regiao2_complemento_reverso.find(
-                            primer2)  # encontrar o desenho do primer2 dentro da lista_regiao2
+                        encontrar_primer2 = lista_regiao2_complemento_reverso.find(primer2)  # encontrar o desenho do primer2 dentro da lista_regiao2
                         tamanho_primer2 = len(primer2)  # determinar o tamanho do primer2
                         ultimo_primer2 = primer2[-1]  # determinar se o primer2 termina com G ou C, ou GC, ou CG
                         composicao_c_primer2 = primer2.count("C")  # determinar a composição de citosina do primer2
                         composicao_g_primer2 = primer2.count("G")  # determinar a composição de guanina do primer2
                         composicao_t_primer2 = primer2.count("T")  # determinar a composição de timina do primer2
                         composicao_a_primer2 = primer2.count("A")  # determinar a composição de adenina do primer2
-                        porcentagem_gc_primer2 = (
-                                                             composicao_c_primer2 + composicao_g_primer2) / tamanho_primer2 * 100  # determinar a composição de G+C do primer2
-                        temperatura_melting_primer2 = (4 * (composicao_c_primer2 + composicao_g_primer2)) + (2 * (
-                                    composicao_a_primer2 + composicao_t_primer2))  # determinar a temperatura de melting (Tm) do primer2
+                        porcentagem_gc_primer2 = (composicao_c_primer2 + composicao_g_primer2) / tamanho_primer2 * 100  # determinar a composição de G+C do primer2
+                        temperatura_melting_primer2 = (4 * (composicao_c_primer2 + composicao_g_primer2)) + (2 * (composicao_a_primer2 + composicao_t_primer2))  # determinar a temperatura de melting (Tm) do primer2
                         temperatura_min_hibridizacao_primer2 = temperatura_melting_primer2 - 5  # determinar a temperatura de hibridização (Ta) do primer2
                         if encontrar_primer2 >= 0:
                             print('''
--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------------------------------------------------------
 
 O seu primer REVERSE:
 ''')
@@ -205,7 +201,7 @@ O seu primer REVERSE:
                                             print('''
 O primer REVERSE também está OK!
 
--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------------------------------------------------------
 
 ÚLTIMO TESTE
 
@@ -213,13 +209,11 @@ Compatibilidade entre as temperaturas de melting dos primers FORWARD e REVERSE..
 
 Testando...''')
                                             compatibilidade_temperatura_melting_primers = temperatura_melting_primer1 - temperatura_melting_primer2  # determinar compatibilidade entre as Ta do primer1 e primer2
-                                            compatibilidade_temperatura_melting_primers_abs = abs(
-                                                compatibilidade_temperatura_melting_primers)  # valor positivo da compatibilidade entre as Ta do primer1 e primer2
+                                            compatibilidade_temperatura_melting_primers_abs = abs(compatibilidade_temperatura_melting_primers)  # valor positivo da compatibilidade entre as Ta do primer1 e primer2
                                             if compatibilidade_temperatura_melting_primers_abs <= 2:
                                                 print('''
 Está Ótimo!
-Os primers FORWARD e REVERSE apresentam temperaturas de melting %.1f°C e %.1f°C respectivamente.''' % (temperatura_melting_primer1, temperatura_melting_primer2))
-                                                print('''
+Os primers FORWARD e REVERSE apresentam temperaturas de melting %.1f°C e %.1f°C respectivamente.
 
 Infelizmente este programinha (ainda) não consegue testar HAIRPIN, SELF-DIMER e PRIMER-DIMER :(
 Essa aplicação tem como finalidade exemplificar de forma didática sobre as regras gerais para o desenho de primer.
@@ -227,11 +221,11 @@ Essa aplicação tem como finalidade exemplificar de forma didática sobre as re
 Espero que tenha ajudado!
 
 
--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------------------------------------------------------
 
-                                                                                             analise-primer v0.6                                                                                             
+                                                            analise-primer v0.7                                                            
 
--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------------------------------------------------------
 
 2019
 Desenvolvido com fins didáticos para o Minicurso BIOLOGIA MOLECULAR: DESENHO DE PRIMERS PARA PCR (VIII Simpósio de Biomedicina da Bahiana)
@@ -241,16 +235,16 @@ http://lattes.cnpq.br/7097758558494370 # lattes
 https://slides.com/lpmor22 # decks
 https://github.com/lpmor22 # github
 laisepaixao@live.com # email
-@lpmor22 # instagram, twitter
+@lpmor22 # instagram
+@lpmor22_ # twitter
 
--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------------------------------------------------------
 
--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------''')
-                                                break
+-------------------------------------------------------------------------------------------------------------------------------------------''' % (temperatura_melting_primer1, temperatura_melting_primer2))
+                                                primer_tests = False
                                             else:
                                                 print('''Os primers FORWARD e REVERSE não apresentam temperaturas compatíveis entre si (%.1f°C e %.1f°C respectivamente).
 Por favor, reveja os critérios para a construção de primers e tente novamente!''' % (temperatura_melting_primer1, temperatura_melting_primer2))
-
                                         else:
                                             print('''
 Talvez você tenha digitado errado ou o primer REVERSE não esteja bom.
@@ -301,3 +295,7 @@ Por favor, reveja os critérios de desenho de primer e tente novamente!''' % tam
 Talvez você tenha digitado errado ou o primer FORWARD não esteja bom.
 Neste caso, seu primer FORWARD não hibridiza com a região de interesse.
 Por favor, reveja os critérios de desenho de primer e tente novamente!''')
+
+input('''
+Pressione QUALQUER TECLA para sair ;) ''')
+sys.exit()
